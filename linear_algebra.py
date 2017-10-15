7c74b4a3ed577110c342070f2d95c969c75a808b
FeherBalazs <feher.balazs.feher@gmail.com>
2017-10-15 19:25:01 +0100

    Extended

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 # What mathematical ideas you need to know to program neural networks?
 # First you need to know some linear algebra.
@@ -16,13 +16,59 @@ for i in range(len(x)):
 print(product)
 
 # Solution 2: Linear algebra version
-x = numpy.array([1,2,3])
-y = numpy.array([2,3,4])
+x = np.array([1,2,3])
+y = np.array([2,3,4])
 print(x * y)
 
 # Solution 2 is 3 times faster. It is also more elegant.
 
 # In neural networks knowledge is stored in connections between neurons, i.e. in synapses. Each synapse is represented by a number. 
 # The larger the number the stronger the connection between the neurons. The stronger the connection the more one neuron can excite the other.
+
+# Vectors
+
+# In the following example let's assume x represents output values of 3 neurons, and w repesents synaptic weights through which those signals reach a certain neuron represented by n.
+# How do we calculate the strength of the signal reaching neuron n?
+
+x = np.array([1,2,3])
+w = np.array([2,3,4])
+n = np.dot(x,w)
+print(n)
+
+# Matrices
+
+a = np.array([
+ [1,2,3], 
+ [4,5,6]
+])
+print(a.shape)
+
+b = np.array([
+ [1,2], 
+ [3,4],
+ [5,6]
+])
+print(b.shape)
+
+# First term of shape function is rows second is columns.
+
+# In the following example let's assume x represents output values of 3 neurons, and w repesents synaptic weights through which those signals reach two neurons represented by n1 and n2.
+# How do we calculate the strength of the signals reaching neurons n1 and n2?
+
+x = np.array([1,2,3])
+w = np.array([
+ [1,2], 
+ [3,4],
+ [5,6]
+])
+n1_n2 = np.dot(x, w)
+print(n1_n2)
+
+# Before training neural nets we set up weight matrices randomly. Let's wtite a function to initialize a weight matrix for the above example? 
+
+def weight_init(input, output):
+	return np.random.random((input, output))
+
+print weight_init(3,2)
 
 # print('''\n-------------------\nSection 2\n-------------------\n''')
